3b5ee5abe012172015fb0a6c3c140d2f85d52231
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-05 15:08:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -12,12 +12,11 @@ char con(char a){
     int b;
     b=(int)a;
     int c=0;
-
     if (b >= 97 && b<=122){
         c = b-32;
     }
     else if(b>=65 && b<=90)
-        c = b + 32;
+        c = b+32;
         return c;
 }
 
