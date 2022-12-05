97038718cc1b49e8a23274ef3209cd3620d495ff
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-05 15:02:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include "lib.h"
+
+
+bool ver(char a){
+    int b;
+    b=(int)a;
+    if (b>=65 & b<=90 || b>=97 & b<=122)
+        return true;
+    else return false;
+}
+char con(char a){
+    int b;
+    b=(int)a;
+    int c=0;
+
+    if (b >= 97 && b<=122){
+        c = b-32;
+    }
+    else if(b>=65 && b<=90)
+        c = b + 32;
+        return c;
+}
 
